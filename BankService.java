6d22c5711755d365696ef074d4814cd372b5f8dd
RajivRanganath <rajivranganath3@gmail.com>
2026-04-19 19:58:54 +0530

    Add files via upload

---
+++ BankService.java
@@ -0,0 +1,8 @@
+public class BankService {
+    public static void main(String[] args) {
+        double balance = 5000;
+        balance += 700;
+        balance -= 100;
+        System.out.println("Final Balance: " + balance);
+    }
+}
